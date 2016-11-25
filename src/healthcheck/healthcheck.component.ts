b95fe7a8e3a6efc417e6a4cdd1116d664c1ffcf2
Raja Sharan Mamidala <rajasharan@gmail.com>
2016-11-25 15:19:08 -0500

    add svg animation

--- src/healthcheck/healthcheck.component.ts
+++ src/healthcheck/healthcheck.component.ts
@@ -6,6 +6,7 @@ import 'rxjs/add/operator/do';
 import 'rxjs/add/operator/delay';
 import 'rxjs/add/operator/retryWhen';
 import 'rxjs/add/operator/repeatWhen';
+import 'rxjs/add/operator/takeWhile';
 
 @Component({
   selector: 'health-check',
@@ -17,36 +18,91 @@ export class HealthCheckComponent implements OnInit, OnDestroy {
   @Input() pingIntervalMilli: number;
   @Input() title: string;
 
-  private timer: Observable<number>;
-  private pingObservable: Observable<Response>;
-  private subscription: Subscription;
+  private httpSubscription: Subscription;
+  private svgSubscription: Subscription;
+
   private currentStatus: number | string;
   private success: number;
   private failure: number;
 
+  private startAngle: number;
+  private endAngle: number;
+  private radius: number;
+  private cx: number;
+  private cy: number;
+  private width: number;
+  private color: string;
+  private frameTime: number;
+
+  private RED: string = "#AA0000";
+  private GREEN: string = "#00AA00";
+
+
   constructor(private http: Http) {
     this.pingIntervalMilli = 3000;
-    this.timer = Observable.timer(0, this.pingIntervalMilli);
     this.success = 0;
     this.failure = 0;
+
+    this.startAngle = 1;
+    this.endAngle = 360;
+    this.cx = this.cy = 150;
+    this.radius = 100;
+    this.width = 40;
+    this.color = this.RED;
+    this.frameTime = 32
   }
 
   ngOnInit(): void {
-    this.subscription = this.http.get(this.pingUrl)
-      .repeatWhen(() => this.timer)
+    this.httpSubscription = this.http.get(this.pingUrl)
+      .repeatWhen(() => Observable.timer(0, this.pingIntervalMilli))
       .retryWhen(err => {
         return err.do(res => {
           this.currentStatus = res.status === 0? "Error": res.status;
+          this.color = this.RED;
           this.failure++;
         }).delay(this.pingIntervalMilli)
       })
       .subscribe(res => {
-        this.currentStatus = res.status
+        this.currentStatus = res.status;
+        this.color = this.GREEN;
         this.success++;
       });
+
+      this.svgSubscription = Observable.timer(0, this.frameTime)
+        .takeWhile(val => val <= this.pingIntervalMilli/16)
+        .repeatWhen(() => Observable.timer(0, this.pingIntervalMilli))
+        .subscribe(() => {
+          this.startAngle = this.startAngle +  Math.round((this.frameTime * 360)/this.pingIntervalMilli);
+          if (this.startAngle > 360) {
+            this.startAngle = this.startAngle - 360;
+          }
+        });
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();
+    this.httpSubscription.unsubscribe();
+    this.svgSubscription.unsubscribe();
+  }
+
+  private describeArc(): string {
+    var start = this.polarToCartesian(this.cx, this.cy, this.radius, this.endAngle);
+    var end = this.polarToCartesian(this.cx, this.cy, this.radius, this.startAngle);
+
+    var largeArcFlag = this.endAngle - this.startAngle <= 180 ? "0" : "1";
+
+    var d = [
+        "M", start.x, start.y, 
+        "A", this.radius, this.radius, 0, largeArcFlag, 0, end.x, end.y
+    ].join(" ");
+
+    return d;   
+  }
+
+  private polarToCartesian(centerX, centerY, radius, angleInDegrees) {
+    var angleInRadians = (angleInDegrees-90) * Math.PI / 180.0;
+    return {
+      x: centerX + (radius * Math.cos(angleInRadians)),
+      y: centerY + (radius * Math.sin(angleInRadians))
+    };
   }
 }
